c95d547f4c30f48ce425ba45d7df016eec8c3e35
lawrennd <lawrennd@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2010-06-07 15:56:27 +0000

    Removed PMVU (moved to MEU).

--- matlab/dimredPlotSquaredDistances.m
+++ matlab/dimredPlotSquaredDistances.m
@@ -1,4 +1,4 @@
-function [v, handle] = dimredPlotSquaredDistances(Y, plotName, ax)
+function [v, tv, handle] = dimredPlotSquaredDistances(Y, plotName, ax, plotWidth)
 
 % DIMREDPLOTSQUAREDDISTANCES Helper function for plotting interpoint distances.
 % FORMAT
@@ -8,21 +8,26 @@ function [v, handle] = dimredPlotSquaredDistances(Y, plotName, ax)
 % ARG Y : the data to plot.
 % ARG plotName : the plot name.
 % ARG ax : the axes to place the plot on.
+% ARG plotWidth : the plot width.
 % RETURN d2 : the squared distances vector.
+% RETURN tv : the theoretical interpoint distances variance (if data is independent).
 % RETURN handle : handle to the axes used.
 %
-% COPYRIGHT : Neil D. Lawrence, 2009
+% COPYRIGHT : Neil D. Lawrence, 2009, 2010
 %
  
 % DIMRED
   
   if nargin < 3
-    ax = gca;
+    ax = [];
     if nargin < 2
       plotName = 'none';
     end
   end
 
+  if isempty(ax)
+    ax = gca;
+  end
   % normalise data to be variance 1 for each dimension.
   varY = var(Y);
   stdY = sqrt(varY);
@@ -38,28 +43,44 @@ function [v, handle] = dimredPlotSquaredDistances(Y, plotName, ax)
 
   % Scale distance by dimension.
   v = v/size(Y, 2);
-
+  tv = (size(Y, 2)/2)/(size(Y, 2)^2/16); % theoretical variance (if independent)
+  
   fprintf([plotName ' empirical variance %2.4f\n'], var(v));
-  fprintf([plotName ' theoretical variance %2.4e\n'], (size(Y, 2)/2)/(size(Y, 2)^2/16));
+  fprintf([plotName ' theoretical variance %2.4e\n'], tv);
 
   % Work out values of bar plot.
   [vals, x] = hist(v, 20);
   vals = vals/(sum(vals)*mean(x(2:end) - x(1:end-1) ));
-
-  bar(ax, x, vals);
+  if isoctave
+    axes(ax);
+    bar(x, vals);
+  else
+    bar(ax, x, vals);
+  end
   hold on
-  a = xlabel(ax, 'squared distance');
+  if isoctave
+    a = xlabel('squared distance');
+  else
+    a = xlabel(ax, 'squared distance');
+  end
   %  set(a, 'fontname', fontName);
   set(a, 'fontsize', 25);
   x = linspace(0, 6, 100);
-  ha = plot(ax, x, gammaPdf(x, size(Y, 2)/2, size(Y, 2)/4), 'k-');
+  if isoctave
+    ha = plot(x, gammaPdf(x, size(Y, 2)/2, size(Y, 2)/4), 'k-');
+  else
+    ha = plot(ax, x, gammaPdf(x, size(Y, 2)/2, size(Y, 2)/4), 'k-');
+  end
   set(ha, 'linewidth', 3);
   set(ax, 'xlim', [0, 6]);
-  set(ax, 'fontname', fontName)
-  set(ax, 'fontsize', 20)
 
-  printPlot(plotName, '../tex/diagrams/', '../html/')
-
+  printLatexPlot(plotName, '../../../dimred/tex/diagrams/', plotWidth);
+  printLatexText(['\global\long\def\captionInfo{$\dataDim=' ...
+                  num2str(size(Y, 2)) '$ (variance of ' ...
+                  'squared distances is $' num2str(var(v), 3) '$ ' ...
+                  'vs predicted $' num2str(tv, 3) '$)}'], [plotName ...
+                  'Caption.tex'], '../../../dimred/tex/diagrams/');
+  
   handle = gcf;
   
 end
